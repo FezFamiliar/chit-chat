16de5e3d47f33d25934d9c7251900456f6e32cfa
Aaron Pop <FezFamiliar@yahoo.com>
2020-03-03 16:45:49 +0200

    update

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,12 @@ class PagesController extends Controller
 
     	return view('home');
     }
+
+
+
+    public function RedirectTest(){
+
+    	return redirect('/')->with('info','Signed up successfully!');
+    	
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/', 'PagesController@RenderHomePage');
+Route::get('/alert', 'PagesController@RedirectTest');
