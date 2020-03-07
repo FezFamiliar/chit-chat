5f7885d45d696da9010d9192baf1c52ee41363b3
Aaron Pop <FezFamiliar@yahoo.com>
2020-03-07 22:57:27 +0200

    added gravatar functionality

--- resources/views/inc/userblock.blade.php
+++ resources/views/inc/userblock.blade.php
@@ -1,6 +1,6 @@
 <div class="media">
 	<a href="" class="pull-left">
-		<img src="{{ asset('img/avatar.jpg') }}" alt="palceholder" width="75" height="100" class="media-object">
+		<img src="https://www.gravatar.com/avatar/{{ md5($user->email) }}?d=mm" alt="palceholder" class="media-object">
 	</a>
 	<div class="media-body">
 			<h4 class="media-heading"><a href=""> {{ $user->name }}</a></h4>
