1f579647e1e37890218702a6fecfb5c1cd67e897
Aaron Pop <aron.pop@sepa.at>
2020-04-17 12:52:04 +0300

    persistent settings with sessions as well

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -15,8 +15,16 @@ class SettingsController extends Controller
     public function getSettings()
     {
 
-    	$settings = DB::table('settings')->get();
 
+        $settings = DB::select(DB::raw('SELECT settings.id, setting_id,user_id,body FROM `settings` LEFT JOIN `settings_state` ON settings.id = settings_state.setting_id ORDER BY settings.id ASC'));
+
+    	//$settings = DB::table('settings')->get();
+
+
+        // echo '<pre>';
+        // print_r($settings);
+        // echo '</pre>';
+        // die();
     	return view('settings.settings')->with('settings',$settings);
     }
 
@@ -31,16 +39,19 @@ class SettingsController extends Controller
  
         if($toggle !=  0) 
         {
+           DB::table('settings_state')->insert(['user_id' => Auth::user()->id, 'setting_id' => $s_id, 'created_at' => NOW(), 'updated_at' => NOW()]);
            $request->session()->push('settings', $s_id);
         }
     	else  
         {
+
+            DB::delete('DELETE FROM settings_state WHERE setting_id = ' . $s_id . ' AND user_id = ' . Auth::user()->id);
             foreach (Session::get('settings') as $key => $value)
             {
-                    if($value ==  $s_id)
-                    {
-                        $request->session()->forget('settings.'.$key);
-                    }
+                if($value ==  $s_id)
+                {
+                    $request->session()->forget('settings.'.$key);
+                }
             }
         }
         
--- resources/views/inc/checkbox.blade.php
+++ resources/views/inc/checkbox.blade.php
@@ -1,7 +1,7 @@
 <div class="toggle">
 	<input type="checkbox" style="display:none;" data-id="{{ $setting->id }}">
 
-	<label for="toggle_k90" class="{{ ((Session::get('settings') != null)) ? (in_array($setting->id, Session::get('settings'))) ? 'true_90' : '' : ''}}">
+	<label for="toggle_k90" class="{{ ($setting->setting_id != null && $setting->user_id == Auth::user()->id && $setting->setting_id == $setting->id) ? 'true_90' : ''}}">
 		<div class="toggle_i">Yes</div>
 		<div class="toggle_j"></div>
 		<div class="toggle_k">No</div>
